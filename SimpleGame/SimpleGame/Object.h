64902466c49322331cdffceacd9aff131107bbe0
Yeo Do Hyun <yeodo95@kpu.ac.kr>
2017-10-13 11:26:36 +0900

    practice 3

--- SimpleGame/SimpleGame/Object.h
+++ SimpleGame/SimpleGame/Object.h
@@ -1,10 +1,11 @@
 #pragma once
+#include <random>
 class Object
 {
 private:
 public:
 	float x, y, z, r, g, b, size, a;
-	int Xs = 0, Ys = 0;
+	int Xs = rand()%2, Ys = rand()%2;
 	Object();
 	Object(int x,int y);
 	~Object();
