6033a02a3d22e477b11de214e7cc397673f7a168
Yeo Do Hyun <yeodo95@kpu.ac.kr>
2017-11-16 18:27:57 +0900

    1

---
+++ SimpleGame/SimpleGame/Object.cpp
@@ -0,0 +1,172 @@
+
+#include "stdafx.h"
+#include "Object.h"
+#include "Renderer.h"
+
+Object::Object()
+{
+}
+
+Object::Object(float x, float y, int Type)
+{
+	posX = x;
+	posY = y;
+	ObjectType = Type;
+
+
+	if (ObjectType == OBJECT_CHARACTER)
+	{
+		vX = 200.f *(((float)std::rand() / (float)RAND_MAX) - 1.5f);
+		vY = 200.f *(((float)std::rand() / (float)RAND_MAX) - 1.5f);
+
+		size = 15;
+		color[0] = 1;
+		color[1] = 1;
+		color[2] = 1;
+		color[3] = 1;
+
+		ShooterIndex = -1;
+		ArrowTimer = 0;
+		Life = 10;
+		Lifetimer = 1000.0f;
+	}
+	else if (ObjectType == OBJECT_BUILDING)
+	{
+
+		size = 80;
+		color[0] = 1;
+		color[1] = 1;
+		color[2] = 0;
+		color[3] = 1;
+
+		Life = 500;
+		Lifetimer = 500.0f;
+
+		BulletTimer = 0.0f;
+	}
+	else if (ObjectType == OBJECT_BULLET)
+	{
+		vX = 600.f *(((float)std::rand() / (float)RAND_MAX) - 1.5f);
+		vY = 600.f *(((float)std::rand() / (float)RAND_MAX) - 1.5f);
+		
+		if (std::rand() % 2 == 1)
+			vX = -vX;
+		if (std::rand() % 2 == 1)
+			vY = -vY;
+
+		size = 7;
+		color[0] = 1;
+		color[1] = 0;
+		color[2] = 0;
+		color[3] = 1;
+
+		Life = 20;
+		Lifetimer = 500.0f;
+
+		BulletTimer = 0.0f;
+	}
+}
+
+//Arrow ¸¸µé±â
+Object::Object(float x, float y, int Type, int index, int life)
+{
+	posX = x;
+	posY = y;
+	ObjectType = Type;
+
+	if (ObjectType == OBJECT_ARROW)
+	{
+		vX = 200.f *(((float)std::rand() / (float)RAND_MAX) - 1.5f);
+		vY = 200.f *(((float)std::rand() / (float)RAND_MAX) - 1.5f);
+
+		if (std::rand() % 2 == 1)
+			vX = -vX;
+		if (std::rand() % 2 == 1)
+			vY = -vY;
+		size = 5;
+		color[0] = 0;
+		color[1] = 1;
+		color[2] = 0;
+		color[3] = 1;
+
+		Life = life;
+		Lifetimer = 500.0f;
+		ShooterIndex = index;
+	}
+}
+
+Object::~Object()
+{
+}
+
+void Object::Update(float elapsedTime)
+{
+	float elapsedTimeInSecond = elapsedTime / 1000.f;
+	
+	if (ObjectType == OBJECT_BUILDING)
+		BulletTimer += elapsedTimeInSecond;
+	else if (ObjectType == OBJECT_CHARACTER)
+		ArrowTimer += elapsedTimeInSecond;
+	
+	if (ObjectType != OBJECT_BUILDING)
+	{
+		Lifetimer -= 0.02f;
+		//std::cout << Lifetimer << std::endl;
+		posX = posX + vX * elapsedTimeInSecond;
+		posY = posY + vY * elapsedTimeInSecond;
+
+		if (posX > 250)
+		{
+			posX = 250;
+
+			if (ObjectType == OBJECT_BULLET || ObjectType == OBJECT_ARROW)
+			{
+				Life = 0.f;
+				std::cout << "delete" << std::endl;
+			}
+
+			vX = -vX;
+		}
+
+		if (posX < -250)
+		{
+			posX = -250;
+
+			if (ObjectType == OBJECT_BULLET || ObjectType == OBJECT_ARROW)
+			{
+				Life = 0.f;
+			}
+
+			vX = -vX;
+		}
+
+		if (posY > 250)
+		{
+			posY = 250;
+
+			if (ObjectType == OBJECT_BULLET || ObjectType == OBJECT_ARROW)
+			{
+				Life = 0.f;
+			}
+
+			vY = -vY;
+		}
+
+		if (posY < -250)
+		{
+			posY = -250;
+
+			if (ObjectType == OBJECT_BULLET || ObjectType == OBJECT_ARROW)
+			{
+				Life = 0.f;
+			}
+
+			vY = -vY;
+		}
+	}
+}
+
+float Object::GetTime()
+{
+	return Lifetimer;
+}
---
+++ SimpleGame/SimpleGame/SceneMgr.cpp
@@ -0,0 +1,207 @@
+#include "stdafx.h"
+#include "SceneMgr.h"
+
+
+SceneMgr::SceneMgr(int width, int height)
+{
+	m_renderer = new Renderer(width, height);
+
+	m_windowWidth = width;
+	m_windowHeight = height;
+
+	for (int i = 0; i < MAX_OBJECTS_COUNT; i++)
+	{
+		m_Objects[i] = NULL;
+	}
+		m_Objects[MAX_OBJECTS_COUNT - 1] = new Object(0, 0, OBJECT_BUILDING);
+		AddObject(m_Objects[MAX_OBJECTS_COUNT - 1]->posX, m_Objects[MAX_OBJECTS_COUNT - 1]->posY, OBJECT_BULLET);
+
+}
+
+
+SceneMgr::~SceneMgr()
+{
+}
+
+void SceneMgr::DrawAllObjects()
+{
+	for (int i = 0; i < MAX_OBJECTS_COUNT; i++)
+	{
+		if (m_Objects[i] != NULL)
+		{
+			if (m_Objects[i]->ObjectType == OBJECT_BUILDING)
+			{
+				GLuint texture = m_renderer->CreatePngTexture("./Textures/ClashRoyale.png");
+
+				m_renderer->DrawTexturedRect(
+					m_Objects[i]->posX, m_Objects[i]->posY, 0, m_Objects[i]->size,
+					m_Objects[i]->color[0], m_Objects[i]->color[1], m_Objects[i]->color[2], m_Objects[i]->color[3], texture);
+			}
+			else
+			{
+				m_renderer->DrawSolidRect(
+					m_Objects[i]->posX, m_Objects[i]->posY, 0, m_Objects[i]->size,
+					m_Objects[i]->color[0], m_Objects[i]->color[1], m_Objects[i]->color[2], m_Objects[i]->color[3]);
+			}
+		}
+	}
+}
+
+void SceneMgr::UpateSceneMgr(float elapsedTime)
+{
+
+	if (m_Objects[MAX_OBJECTS_COUNT - 1]->BulletTimer > 0.5f)
+	{
+		AddObject(m_Objects[MAX_OBJECTS_COUNT - 1]->posX, m_Objects[MAX_OBJECTS_COUNT - 1]->posY, OBJECT_BULLET);
+		m_Objects[MAX_OBJECTS_COUNT - 1]->BulletTimer = 0;
+	}
+	for (int i = 0; i < MAX_OBJECTS_COUNT; i++)
+	{
+		if (m_Objects[i] != NULL)
+		{
+			if (m_Objects[i]->ObjectType == OBJECT_CHARACTER && m_Objects[i]->ArrowTimer > 0.5f)
+			{
+				std::cout << m_Objects[i]->ArrowTimer << std::endl;
+				AddObject(m_Objects[i]->posX, m_Objects[i]->posY, OBJECT_ARROW,i);
+				m_Objects[i]->ArrowTimer = 0;
+			}
+			if (m_Objects[i]->GetTime() <= 0.0f)
+			{
+				m_Objects[i] = NULL;
+				delete m_Objects[i];
+				continue;
+			}
+			else
+				m_Objects[i]->Update(elapsedTime);
+
+			if (m_Objects[i]->Life <= 0)
+			{
+				m_Objects[i] = NULL;
+				delete m_Objects[i];
+			}
+		}
+	}
+	ColisionTest();
+}
+
+void SceneMgr::AddObject(float x, float y, int type)
+{
+	for (int i = 0; i < MAX_OBJECTS_COUNT; i++)
+	{
+		if (type == OBJECT_CHARACTER && m_Objects[i] == NULL)
+		{
+			m_Objects[i] = new Object(x, y, type);
+			break;
+		}
+		if (type == OBJECT_BULLET && m_Objects[i] == NULL)
+		{
+			m_Objects[i] = new Object(x, y, type);
+			break;
+		}
+	}
+
+}
+
+void SceneMgr::AddObject(float x, float y, int type, int index)
+{
+	for (int i = 0; i < MAX_OBJECTS_COUNT; i++)
+	{
+		if (type == OBJECT_ARROW && m_Objects[i] == NULL)
+		{
+			m_Objects[i] = new Object(x, y, type, index, m_Objects[index]->Life);
+			break;
+		}
+	}
+
+}
+void SceneMgr::ColisionTest()
+{
+
+	for (int i = 0; i < MAX_OBJECTS_COUNT; i++)
+	{
+		if (m_Objects[i] != NULL)
+		{
+			if (m_Objects[i]->ObjectType == OBJECT_CHARACTER)
+			{
+				m_Objects[i]->color[0] = 1;
+				m_Objects[i]->color[1] = 1;
+				m_Objects[i]->color[2] = 1;
+				m_Objects[i]->color[3] = 1;
+			}
+			else if (m_Objects[i]->ObjectType == OBJECT_BUILDING)
+			{
+				m_Objects[i]->color[0] = 1;
+				m_Objects[i]->color[1] = 1;
+				m_Objects[i]->color[2] = 0;
+				m_Objects[i]->color[3] = 1;
+			}
+			else if (m_Objects[i]->ObjectType == OBJECT_BULLET)
+			{
+				m_Objects[i]->color[0] = 1;
+				m_Objects[i]->color[1] = 0;
+				m_Objects[i]->color[2] = 0;
+				m_Objects[i]->color[3] = 1;
+			}
+
+		}
+		for (int j = 0; j < MAX_OBJECTS_COUNT; j++)
+		{
+			if (i == j )
+				continue;
+			float min_iX, max_iX, min_jX, max_jX;
+			float min_iY, max_iY, min_jY, max_jY;
+
+			if (m_Objects[i] != NULL&&m_Objects[j] != NULL)
+			{
+				if (m_Objects[i]->ObjectType == m_Objects[j]->ObjectType)
+					continue;
+				min_iX = (m_Objects[i]->posX) - (m_Objects[i]->size / 2);
+				max_iX = (m_Objects[i]->posX) + (m_Objects[i]->size / 2);
+				min_iY = (m_Objects[i]->posY) - (m_Objects[i]->size / 2);
+				max_iY = (m_Objects[i]->posY) + (m_Objects[i]->size / 2);
+
+				min_jX = (m_Objects[j]->posX) - (m_Objects[j]->size / 2);
+				max_jX = (m_Objects[j]->posX) + (m_Objects[j]->size / 2);
+				min_jY = (m_Objects[j]->posY) - (m_Objects[j]->size / 2);
+				max_jY = (m_Objects[j]->posY) + (m_Objects[j]->size / 2);
+
+				if (min_iX<max_jX && max_iX>min_jX && min_iY<max_jY&&max_iY>min_jY)
+				{
+					if (m_Objects[i]->ObjectType == OBJECT_BUILDING &&m_Objects[j]->ObjectType == OBJECT_CHARACTER)
+					{
+						m_Objects[i]->Life -= m_Objects[j]->Life;
+						std::cout << m_Objects[i]->Life << std::endl;
+						m_Objects[i]->color[0] = 1;
+						m_Objects[i]->color[1] = 0;
+						m_Objects[i]->color[2] = 0;
+						m_Objects[i]->color[3] = 1;
+						m_Objects[j] = NULL;
+						delete m_Objects[j];
+					}
+					else if (m_Objects[i]->ObjectType == OBJECT_CHARACTER &&m_Objects[j]->ObjectType == OBJECT_BULLET)
+					{
+						m_Objects[i]->Life -= m_Objects[j]->Life;
+						std::cout << m_Objects[i]->Life << std::endl;
+						m_Objects[i]->color[0] = 1;
+						m_Objects[i]->color[1] = 0;
+						m_Objects[i]->color[2] = 0;
+						m_Objects[i]->color[3] = 1;
+						m_Objects[j] = NULL;
+						delete m_Objects[j];
+					}
+					else if ((m_Objects[i]->ObjectType == OBJECT_CHARACTER || m_Objects[i]->ObjectType == OBJECT_BUILDING) &&m_Objects[j]->ObjectType == OBJECT_ARROW && i != m_Objects[j]->ShooterIndex)
+					{
+						m_Objects[i]->Life -= m_Objects[j]->Life;
+						std::cout << m_Objects[i]->Life << std::endl;
+						m_Objects[i]->color[0] = 1;
+						m_Objects[i]->color[1] = 0;
+						m_Objects[i]->color[2] = 0;
+						m_Objects[i]->color[3] = 1;
+						m_Objects[j] = NULL;
+						delete m_Objects[j];
+					}
+				}
+			}
+		}
+	}
+}
