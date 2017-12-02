cac9dc6f557c317fd94892bc5c73d1508ad246b8
Yeo Do Hyun <yeodo95@kpu.ac.kr>
2017-12-02 15:03:01 +0900

    Lecture 10

--- SimpleGame/SimpleGame/SceneMgr.h
+++ SimpleGame/SimpleGame/SceneMgr.h
@@ -18,10 +18,13 @@ public:
 	SceneMgr(int width, int height);
 	~SceneMgr();
 
+	int CharacterMove = 0;
+
 	void DrawAllObjects();
 	void UpateSceneMgr(float elapsedTime);
 
 	void AddObject(float x, float y, int type,  int team);
 	void ColisionTest();
+
 };
 
